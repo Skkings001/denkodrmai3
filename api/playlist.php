49636088befc92b1b416fff8fab2dd49b1f7256c
Skkings001 <skpgamer001@outlook.com>
2024-09-24 00:28:12 +0530

    Update playlist.php

--- api/playlist.php
+++ api/playlist.php
@@ -30,7 +30,7 @@ foreach ($channels as $channel) {
         continue;
     }
     $extension = pathinfo(parse_url($dashUrl, PHP_URL_PATH), PATHINFO_EXTENSION);
-    $playlistUrl = "https://$serverAddress/{$id}.$extension|"
+    $playlistUrl = "https://$serverAddress/{$id}.$extension|X-Forwarded-For=59.178.72.184";
     $m3u8PlaylistFile .= "#EXTINF:-1 tvg-id=\"{$id}\" tvg-country=\"IN\" catchup-days=\"7\" catchup-source="&amp;utc={utc}&amp;lutc={utcend}" tvg-logo=\"https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/{$channel['channel_logo']}\" group-title=\"{$channel['channel_genre'][0]}\",{$channel['channel_name']}\n";
     $m3u8PlaylistFile .= "#KODIPROP:inputstream.adaptive.license_type=clearkey\n";
     $m3u8PlaylistFile .= "#KODIPROP:inputstream.adaptive.license_key=https://play.denver1769.in/KeyLelo4.php?id=$id}\n";
